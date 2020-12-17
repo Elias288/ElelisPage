183fbac6f126ce7bc569a16ae4cb962a97bd098a
Elias Bianchi <elias.bianchi@utec.edu.uy>
2020-12-17 20:29:00 -0300

    Enviar correo terminado

--- enviar.php
+++ enviar.php
@@ -10,14 +10,13 @@ if(isset($_POST['enviar'])){
         $header = "From: " . $mail . "\r\n";
         $header .= "Reply-To: noreply@example.com" . "\r\n";
         $header .= "X-Mailer: PHP/" .phpversion();
-/*
+
         $message = "Este mensaje fue enviado por: " . $name . "\r\n";
         $message .= "Su e-mail es: " . $mail . "\r\n";
         $message .= "Mensaje: " . $_POST['message'] . "\r\n";
-        $message .= "Enviado el: " . date('d/m/Y', time());*/
+        $message .= "Enviado el: " . date('d/m/Y', time());
 
         $mail = mail("bianchi.elias@gmail.com", $asunto, $message, $header);
-        echo "cambiarPestaña(event,'contacto')";
         if($mail){
             echo "<h2>Mail enviado exitosamente</h2>";
         }
