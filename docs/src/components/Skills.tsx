bfd63cb9cb0865c5b8c6b689415a6cef973b4550
Elias288 <bianchi.elias@gmail.com>
2023-06-09 23:29:10 -0300

    Corrección de skills

--- docs/src/components/Skills.tsx
+++ docs/src/components/Skills.tsx
@@ -13,29 +13,39 @@ import { SiPodman, SiPhp, SiCakephp, SiTypescript, SiGnubash } from "react-icons
 
 function Skills() {
     return (
-        <div className="flex flex-col items-center">
-
-            <h1 className="text-center font-bold text-xl uppercase">
-                Skills
-            </h1>
-
-            <div className=" grid grid-cols-3 lg:grid-cols-6 gap-4 w-3/4 pt-2">
-                <div className="flex flex-col items-center"><IoLogoJavascript className="text-5xl" />javascript</div>
-                <div className="flex flex-col items-center"><SiTypescript className="text-5xl" />Typescript</div>
-                <div className="flex flex-col items-center"><IoLogoPython className="text-5xl" />Python</div>
-                <div className="flex flex-col items-center"><IoLogoNodejs className="text-5xl" />Nodejs</div>
-                <div className="flex flex-col items-center"><IoLogoAngular className="text-5xl" />Angular</div>
-                <div className="flex flex-col items-center"><IoLogoReact className="text-5xl" />React</div>
-                <div className="flex flex-col items-center"><GrMysql className="text-5xl" />MYSQL</div>
-                <div className="flex flex-col items-center"><IoLogoHtml5 className="text-5xl" />HTML5</div>
-                <div className="flex flex-col items-center"><IoLogoCss3 className="text-5xl" />Css</div>
-                <div className="flex flex-col items-center"><IoLogoDocker className="text-5xl" />Docker</div>
-                <div className="flex flex-col items-center"><SiPodman className="text-5xl" />Podman</div>
-                <div className="flex flex-col items-center"><SiPhp className="text-5xl" />PHP</div>
-                <div className="flex flex-col items-center"><SiCakephp className="text-5xl" />CakePHP</div>
-                <div className="flex flex-col items-center"><SiGnubash className="text-5xl" />Bash</div>
+        <>
+            <div className="flex flex-col items-center">
+                <h1 className="text-center font-bold text-xl uppercase">
+                    Habilidades
+                </h1>
+                <div className="grid grid-cols-3 lg:grid-cols-6 gap-4 w-3/4 pt-2">
+                    <div className="flex flex-col items-center"><IoLogoJavascript className="text-5xl" />javascript</div>
+                    <div className="flex flex-col items-center"><SiTypescript className="text-5xl" />Typescript</div>
+                    <div className="flex flex-col items-center"><IoLogoPython className="text-5xl" />Python</div>
+                    <div className="flex flex-col items-center"><GrMysql className="text-5xl" />MYSQL</div>
+                    <div className="flex flex-col items-center"><IoLogoHtml5 className="text-5xl" />HTML5</div>
+                    <div className="flex flex-col items-center"><IoLogoCss3 className="text-5xl" />Css</div>
+                    <div className="flex flex-col items-center"><IoLogoDocker className="text-5xl" />Docker</div>
+                    <div className="flex flex-col items-center"><SiPodman className="text-5xl" />Podman</div>
+                    <div className="flex flex-col items-center"><SiPhp className="text-5xl" />PHP</div>
+                    <div className="flex flex-col items-center"><SiGnubash className="text-5xl" />Bash</div>
+                </div>
             </div>
-        </div>
+
+            <hr />
+
+            <div className="flex flex-col items-center">
+                <h1 className="text-center font-bold text-xl uppercase">
+                    Frameworks
+                </h1>
+                <div className="grid grid-cols-3 lg:grid-cols-6 gap-4 w-3/4 pt-2">
+                    <div className="flex flex-col items-center"><IoLogoNodejs className="text-5xl" />Nodejs</div>
+                    <div className="flex flex-col items-center"><IoLogoAngular className="text-5xl" />Angular</div>
+                    <div className="flex flex-col items-center"><IoLogoReact className="text-5xl" />React</div>
+                    <div className="flex flex-col items-center"><SiCakephp className="text-5xl" />CakePHP</div>
+                </div>
+            </div>
+        </>
     );
 }
 
