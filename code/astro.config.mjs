0e3b9cd2e60fc63f7f252175b757d4c3792fbdf7
Elias288 <bianchi.elias@gmail.com>
2025-08-30 14:28:43 -0300

    feat: Deploy 1

--- code/astro.config.mjs
+++ code/astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://elias288.github.io/ElelisPage/",
     outDir: "public",
     publicDir: "static"
 });
