eac45dfb1a40f3506e85d330588eecf8e8659e8d
Elias288 <bianchi.elias@gmail.com>
2025-09-09 11:35:58 -0300

    fix: Restore perfil and fetch projects

--- code/src/utils/page_config.ts
+++ code/src/utils/page_config.ts
@@ -1,6 +1,7 @@
 const config = {
     TitlePage: "Elelis Page",
-    basePage: `${import.meta.env.DEV ? "" : "ElelisPage"}`
+    basePage: `${import.meta.env.DEV ? "" : "ElelisPage"}`,
+    urlPage: "https://elias288.github.io/ElelisPage"
 }
 
 export default config
